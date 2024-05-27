bd8d17a7f22647d371ff2e3fdb70ebc9d4cbea09
Ahmet Esat KAYA <147531578+AhmetEsatKAYA@users.noreply.github.com>
2024-05-27 13:29:14 +0300

    Add files via upload

---
+++ cards.js
@@ -0,0 +1,14 @@
+var cards = [
+    { word: 'Elma', meaning: 'Bir meyve' },
+    { word: 'Bilgisayar', meaning: 'Bir elektronik cihaz' },
+    { word: 'Kitap', meaning: 'Okumak için yazılmış eser' },
+    { word: 'Köpek', meaning: 'Evcil bir hayvan' },
+    { word: 'Araba', meaning: 'Bir ulaşım aracı' },
+    { word: 'Ev', meaning: 'İçinde yaşanılan yapı' },
+    { word: 'Kalem', meaning: 'Yazmak için kullanılan bir araç' },
+    { word: 'Masa', meaning: 'Üzerinde çalışılan mobilya' },
+    { word: 'Telefon', meaning: 'İletişim aracı' },
+    { word: 'Televizyon', meaning: 'Görsel ve işitsel yayın cihazı' },
+    { word: 'Bisiklet', meaning: 'İki tekerlekli ulaşım aracı' },
+    { word: 'Kedi', meaning: 'Evcil bir hayvan' }
+];
--- js.js
+++ js.js
@@ -1,18 +1,3 @@
-var cards = [
-    { word: 'Elma', meaning: 'Bir meyve' },
-    { word: 'Bilgisayar', meaning: 'Bir elektronik cihaz' },
-    { word: 'Kitap', meaning: 'Okumak için yazılmış eser' },
-    { word: 'Köpek', meaning: 'Evcil bir hayvan' },
-    { word: 'Araba', meaning: 'Bir ulaşım aracı' },
-    { word: 'Ev', meaning: 'İçinde yaşanılan yapı' },
-    { word: 'Kalem', meaning: 'Yazmak için kullanılan bir araç' },
-    { word: 'Masa', meaning: 'Üzerinde çalışılan mobilya' },
-    { word: 'Telefon', meaning: 'İletişim aracı' },
-    { word: 'Televizyon', meaning: 'Görsel ve işitsel yayın cihazı' },
-    { word: 'Bisiklet', meaning: 'İki tekerlekli ulaşım aracı' },
-    { word: 'Kedi', meaning: 'Evcil bir hayvan' }
-];
-
 var gameCards = [];
 var flippedCards = [];
 var matchedCards = [];
@@ -21,6 +6,8 @@ var timeElapsed = 0;
 var lockBoard = false;
 
 function startGame() {
+    document.getElementById("oyun").style = "display: block;";
+    document.getElementById("acilis").style = "display: none;";
     prepareGameCards();
     shuffle(gameCards);
     createBoard();
